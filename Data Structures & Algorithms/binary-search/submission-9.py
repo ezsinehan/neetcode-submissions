221a9cdf31d41da92d199265ba9da83bce76cb50
sinehan <122057249+ezsinehan@users.noreply.github.com>
2026-04-27 21:03:00 -0700

    Update: binary-search - submission-9

--- Data Structures & Algorithms/binary-search/submission-9.py
+++ Data Structures & Algorithms/binary-search/submission-9.py
@@ -1,10 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
-        # middle = len(nums) // 2
         right  = len(nums) - 1
 
-        # goal is to shrink array till we reach the potential location of our number
         while left <= right:
             middle = left + (right - left) // 2
             if nums[middle] == target:
